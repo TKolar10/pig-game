1baf8d1bf9b2c30aefbbd7c53b6d31a80a9c1af3
TKolar10 <tomislav114@gmail.com>
2022-04-21 12:46:24 +0200

    update

--- js.js
+++ js.js
@@ -4,12 +4,20 @@ const newGame = document.querySelector(".card__btn-new");
 const imgDice = document.querySelector(".card__img");
 const player1 = document.querySelector(".player-1");
 const player2 = document.querySelector(".player-2");
+const info = document.querySelector(".card__img-instructions");
+const popUp = document.querySelector(".card__info");
 
 init();
 
-rollDice.addEventListener("click", roleDice);
+imgDice.addEventListener("click", roleDice);
 holdScore.addEventListener("click", holdDice);
 newGame.addEventListener("click", init);
+info.addEventListener("click", function() {
+    document.querySelector(".card__info").style.visibility = "visible";
+});
+popUp.addEventListener("click", function() {
+    document.querySelector(".card__info").style.visibility = "hidden";
+});
 
 function init() {
     player1Score = 0;
@@ -54,14 +62,14 @@ function holdDice() {
     if (player === "player-1") {
         player1Score += currentScore;
         playerScore.innerText = player1Score;
-        if (player1Score >= 10) {
+        if (player1Score >= 50) {
             return winner(player);
         }
         switchActivePlayer(currentPlayer);
     } else {
         player2Score += currentScore;
         playerScore.innerText = player2Score;
-        if (player2Score >= 10) {
+        if (player2Score >= 50) {
             return winner(player);
         }
         switchActivePlayer(currentPlayer);
